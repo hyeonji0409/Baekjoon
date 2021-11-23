21e04c451a16a737fd5c5acd2486acb7087556f7
hyeonji0409 <guswl8507@naver.com>
2021-11-23 23:33:19 +0900

    21-11-23

--- src/Baekjoon10950.java
+++ src/Baekjoon10950.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Baekjoon10950 {
+import java.util.Scanner;
+
+public class Baekjoon10950 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+        int t = scan.nextInt();
+
+        for(int i = 0; i<t;i++){
+            a = scan.nextInt();
+            b = scan.nextInt();
+            System.out.println(a+b);
+        }
+    }
 }
--- src/Baekjoon15552.java
+++ src/Baekjoon15552.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Baekjoon15552 {
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Baekjoon15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for(int i = 0; i<T;i++){
+            st = new StringTokenizer(bufferedReader.readLine());
+            bufferedWriter.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+        }
+
+        bufferedWriter.close();
+    }
 }
