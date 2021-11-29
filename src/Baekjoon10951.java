4f8dac00446e375b02f1b81e25ddfa7df7b0eac0
hyeonji0409 <guswl8507@naver.com>
2021-11-29 17:36:12 +0900

    21-11-29

--- src/Baekjoon10951.java
+++ src/Baekjoon10951.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Baekjoon10951 {
+import java.util.Scanner;
+
+public class Baekjoon10951 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while(scan.hasNextInt()){
+            int a, b;
+            a = scan.nextInt();
+            b = scan.nextInt();
+            System.out.println((a+b));
+        }
+
+        scan.close();
+    }
 }
--- src/Baekjoon10952.java
+++ src/Baekjoon10952.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Baekjoon10952 {
+import java.util.Scanner;
+
+public class Baekjoon10952 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b;
+
+        while(true){
+            a = scan.nextInt();
+            b = scan.nextInt();
+
+            if(a == 0 && b == 0){
+                break;
+            }
+            else{
+                System.out.println((a+b));
+            }
+        }
+    }
 }
